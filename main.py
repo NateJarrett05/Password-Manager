c27490b12d9dfd4acb95d1c7b10314d1856191aa
Nate <natejarrett05@gmail.com>
2024-08-31 19:40:22 -0400

    Password Generation.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import psycopg2
 
+import string
+import secrets
 # Connect to an existing database
 conn = psycopg2.connect(
     database = "pass_manager",
@@ -7,6 +9,17 @@ conn = psycopg2.connect(
     password = "password01",
     host = "0.0.0.0"
 )
+
+# Creates a password with at least one upper and lowercase letter + min 3 digits
+def create_password(length):
+    chars = string.ascii_letters + string.digits + '!@#$%^&*'
+    while True:
+        password = ''.join(secrets.choice(chars) for i in range(length))
+        if (any(c.islower() for c in password)
+                and any(c.isupper() for c in password)
+                and sum(c.isdigit() for c in password) >= 3):
+            break
+    return password
 
 # Open cursor to perform database operatiosn
 cur = conn.cursor()
