8c0ee9d35a69276431785cdd31b69bba05eab642
Nate <natejarrett05@gmail.com>
2024-09-04 19:54:28 -0400

    Database manipulation implemented.

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ def create_password(length):
             break
     return password
 
+# Checks the databse for a given URL and returns if it exists
+def check_entry_exists(cursor, URL):
+    SQL = "SELECT * FROM passwords WHERE url = %s"
+    cursor.execute(SQL, (URL,))
+    entries = cursor.fetchone()
+    if entries == None:
+        print("No account found for the URL: " + URL)
+        return False
+    else:
+        return True
+
 # Retrieve and print all entries from the database
 def query_all(cursor):
     cursor.execute("SELECT * FROM passwords")
@@ -42,16 +53,46 @@ def query_one(cursor, URL):
             print("Password: " + record[2])
             print("----------")
 
+# Insert a new entry to the database
+def insert(cursor, URL, username, password):
+    SQL = "INSERT INTO passwords VALUES (%s, %s, %s)"
+    cursor.execute(SQL, (URL, username, password))
+    query_one(cursor, URL)
+    print("Entry succesfully inserted.\n")
+
+# Update an existing entry to the database
+def update(cursor, URL, username, password):
+    if check_entry_exists(cursor, URL):
+        SQL = "UPDATE passwords SET username = %s, password = %s WHERE url = %s"
+        cursor.execute(SQL, (username, password, URL))
+        query_one(cursor, URL)
+        print("Entry succesfully updated.\n")
+
+# Remove an existing entry from the database
+def remove(cursor, URL):
+    if check_entry_exists(cursor, URL):
+        query_one(cursor, URL)
+        SQL = "DELETE FROM passwords WHERE url = %s"
+        cursor.execute(SQL, (URL,))
+        print("Entry succesfully removed.\n")
+
 # Load the config from database.ini
 db_config = config.load_config()
 # Connect to an existing database with the config
 db_connect = connect.connect(db_config);
 # Open cursor to perform database operatiosn
-cur = db_connect.cursor()
+db_cursor = db_connect.cursor()
 
-#Query the database
-query_all(cur)
-query_one(cur, "www.google.com")
+# Query the database
+query_all(db_cursor)
+
+# Modify the database
+insert(db_cursor, "www.youtube.com", "username03", "password03")
+update(db_cursor, "www.youtube.com", "username10", "password10")
+remove(db_cursor, "www.youtube.com")
+
+# Push any changes to the database
+db_connect.commit()
 
 #Close Communication with database
-cur.close()
+db_cursor.close()
